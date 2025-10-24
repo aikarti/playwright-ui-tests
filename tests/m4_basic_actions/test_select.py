c8bb0123c452bb11076ced4d08bf76b2eed494ea
m <karthick.m@capgemini.com>
2025-10-24 18:27:23 +0530

    new test script

--- tests/m4_basic_actions/test_select.py
+++ tests/m4_basic_actions/test_select.py
@@ -1,9 +1,48 @@
-from playwright.sync_api import expect, Page
+from playwright.sync_api import sync_playwright, expect, Page
+from tests.utils.constants import BASE_URL
 
 
 def test_select(page):
-    page.goto(f'savings.html')
+    # Navigate to the savings page
+    page.goto(f'{BASE_URL}/savings.html')   
 
+    # Get the input elements
     deposit = page.get_by_test_id('deposit')
     period = page.get_by_test_id('period')
     result = page.get_by_test_id('result')
+
+    # Select deposit amount
+    deposit.fill('2000')
+    expect(deposit).to_have_value('2000')
+    # Select period
+    period.select_option('1 Year')
+    expect(period).to_have_value('1 Year')
+    # Verify the result
+    expect(result).to_have_text('After 1 Year you will earn $100.00 on your deposit')
+    print("✅ test_select passed successfully.")
+
+
+
+def test_select_one():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)  # 👈 This makes the browser visible
+        page = browser.new_page()
+
+        page.goto("http://localhost:3000/savings.html")
+
+        deposit = page.get_by_test_id('deposit')
+        period = page.get_by_test_id('period')
+        result = page.get_by_test_id('result')
+
+        deposit.fill('2000')
+        expect(deposit).to_have_value('2000')
+
+        #period.fill('1 Year')
+        period.select_option('1 Year')
+        expect(period).to_have_value('1 Year')
+
+        expect(result).to_have_text('After 1 Year you will earn $100.00 on your deposit')
+
+        print("✅ test_select passed successfully.")
+        page.pause
+        #browser.close()
--- tests/m5_advanced_actions/test_dialogs.py
+++ tests/m5_advanced_actions/test_dialogs.py
@@ -1,26 +1,41 @@
-from playwright.sync_api import expect, Page
-
-name = 'Sofia'
+from playwright.sync_api import Playwright, expect, Page
+from tests.utils.constants import BASE_URL
+name = 'Karthick'
 
 
 # Default handling is to dismiss
 def test_dialog_default_handling(page: Page):
-    page.goto('')
+    
+    page.goto(f'{BASE_URL}')
 
-    name_input = page.get_by_label('First name')
-    name_input.fill(name)
-    expect(name_input).to_have_value(name)
+
+    input_firstname = page.get_by_label('First name')
+    input_firstname.fill(name)
+    expect(input_firstname).to_have_value(name)
+
+    input_lastname = page.get_by_label('Last name')
+    input_lastname.fill('Amaze')
+    expect(input_lastname).to_have_value('Amaze')
 
     page.get_by_role('button', name='Clear').click()
-    expect(name_input).to_have_value(name)
+    expect(input_firstname).to_have_value(name)
+    expect(input_lastname).to_have_value('Amaze')
+
+     
 
 
 def test_dialog_ok_or_dismiss(page: Page):
+    page.once('dialog', lambda dialog: dialog.accept())
 
-    page.goto('')
+    page.goto(f'{BASE_URL}')
+    input_firstname = page.get_by_label('First name')
+    input_firstname.fill(name)
 
-    input_name = page.get_by_label('First name')
-    input_name.fill(name)
+    input_lastname = page.get_by_label('Last name')
+    input_lastname.fill('Amaze')
 
     page.get_by_role('button', name='Clear').click()
-    expect(input_name).to_have_value('')
+    expect(input_firstname).to_have_value('')
+    expect(input_lastname).to_have_value('')
+
+    page.pause()
